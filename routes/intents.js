78b5c4a0e6bd8500a2267f0b5d2f2b1a342fb9b7
ran-j <ran-junior@hotmail.com>
2018-08-21 12:54:25 -0300

    Add arrow functions in intents

--- routes/intents.js
+++ routes/intents.js
@@ -2,54 +2,57 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('login');  
 });
 
 router.get('/intents', async (req, res, next) => {  
-  require('../models/intents').find({},function(err,inte){
+  require('../models/intents').find({},(err,inte) =>{
     var intents =  inte.length > 0 ?  inte : require('../Libs/intents');
     res.render('Training/intents', { intents, focus: 1 });  
   }); 
 });
 
 router.get('/synonyms', async (req, res, next) => {  
-  require('../models/synonyms').find({},function(err,synonym){
+  require('../models/synonyms').find({},(err,synonym) =>{
     var synonyms =  synonym.length > 0 ? synonym : require('../Libs/synonyms');
     res.render('Training/synonyms', { synonyms, focus: 2 });  
   }); 
 });
 
 router.get('/edit/:tag', async (req, res, next) => {  
-  require('../models/intents').find({},function(err,inte){
+  require('../models/intents').find({},(err,inte) =>{
     var intents =  inte.length > 0 ?  inte : require('../Libs/intents');
-    intents.forEach(function(intent){
+    intents.forEach((intent) =>{
       if(intent.tag == req.params.tag){
         res.render('Training/edit_intents', { intent, focus: 1 });
       }
     });
+    res.render('404');
   });    
 });
 
 router.get('/training/:tag', async (req, res, next) => {  
-  require('../models/intents').find({},function(err,inte){
+  require('../models/intents').find({},(err,inte) =>{
     var intents =  inte.length > 0 ?  inte : require('../Libs/intents');
-    intents.forEach(function(intent){
+    intents.forEach((intent) =>{
       if(intent.tag == req.params.tag){
         res.render('Training/training', { intent, focus: 1 });
       }
     });
+    res.render('404');
   });    
 });
 
 router.get('/editkey/:keyword', async (req, res, next) => {  
-  require('../models/synonyms').find({},function(err,syn){
+  require('../models/synonyms').find({},(err,syn) =>{
     var synonyms =  syn.length > 0 ?  syn : require('../Libs/synonyms');
-    synonyms.forEach(function(synonym){
+    synonyms.forEach((synonym) =>{
       if(synonym.keyWord == req.params.keyword){
         res.render('Training/synonymsEdit', { synonym, focus: 2 });
       }
     });
+    res.render('404');
   });    
 });
 
