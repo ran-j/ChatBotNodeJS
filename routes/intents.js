23ed0c9fae0aa43d882837f850a8a16e7d2f0bd1
ran-j <ran-junior@hotmail.com>
2018-08-21 13:02:10 -0300

    Add index home

--- routes/intents.js
+++ routes/intents.js
@@ -6,6 +6,10 @@ router.get('/', (req, res, next) => {
   res.render('login');  
 });
 
+router.get('/home', (req, res, next) => {
+  res.render('index',{ focus: 0 });  
+});
+
 router.get('/intents', async (req, res, next) => {  
   require('../models/intents').find({},(err,inte) =>{
     var intents =  inte.length > 0 ?  inte : require('../Libs/intents');
@@ -35,24 +39,28 @@ router.get('/edit/:tag', async (req, res, next) => {
 router.get('/training/:tag', async (req, res, next) => {  
   require('../models/intents').find({},(err,inte) =>{
     var intents =  inte.length > 0 ?  inte : require('../Libs/intents');
+    let found = false;
     intents.forEach((intent) =>{
       if(intent.tag == req.params.tag){
+        found = true;
         res.render('Training/training', { intent, focus: 1 });
       }
     });
-    res.render('404');
+    if(!foundres){res.render('404');}
   });    
 });
 
 router.get('/editkey/:keyword', async (req, res, next) => {  
   require('../models/synonyms').find({},(err,syn) =>{
     var synonyms =  syn.length > 0 ?  syn : require('../Libs/synonyms');
+    let found = false;
     synonyms.forEach((synonym) =>{
       if(synonym.keyWord == req.params.keyword){
+        found = true;
         res.render('Training/synonymsEdit', { synonym, focus: 2 });
       }
     });
-    res.render('404');
+    if(!found){res.render('404');}
   });    
 });
 
