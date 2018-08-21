941d644b5c53e4fda7dcbfb07fb9ad0ac2b8ed29
ran-j <ran-junior@hotmail.com>
2018-08-21 12:49:15 -0300

    Change templates organization and add training template

--- routes/intents.js
+++ routes/intents.js
@@ -9,14 +9,14 @@ router.get('/', function(req, res, next) {
 router.get('/intents', async (req, res, next) => {  
   require('../models/intents').find({},function(err,inte){
     var intents =  inte.length > 0 ?  inte : require('../Libs/intents');
-    res.render('Training/intents', { intents, focus: 0 });  
+    res.render('Training/intents', { intents, focus: 1 });  
   }); 
 });
 
 router.get('/synonyms', async (req, res, next) => {  
   require('../models/synonyms').find({},function(err,synonym){
     var synonyms =  synonym.length > 0 ? synonym : require('../Libs/synonyms');
-    res.render('Training/synonyms', { synonyms, focus: 1 });  
+    res.render('Training/synonyms', { synonyms, focus: 2 });  
   }); 
 });
 
@@ -25,7 +25,29 @@ router.get('/edit/:tag', async (req, res, next) => {
     var intents =  inte.length > 0 ?  inte : require('../Libs/intents');
     intents.forEach(function(intent){
       if(intent.tag == req.params.tag){
-        res.render('Training/edit_intents', { intent, focus: 0 });
+        res.render('Training/edit_intents', { intent, focus: 1 });
+      }
+    });
+  });    
+});
+
+router.get('/training/:tag', async (req, res, next) => {  
+  require('../models/intents').find({},function(err,inte){
+    var intents =  inte.length > 0 ?  inte : require('../Libs/intents');
+    intents.forEach(function(intent){
+      if(intent.tag == req.params.tag){
+        res.render('Training/training', { intent, focus: 1 });
+      }
+    });
+  });    
+});
+
+router.get('/editkey/:keyword', async (req, res, next) => {  
+  require('../models/synonyms').find({},function(err,syn){
+    var synonyms =  syn.length > 0 ?  syn : require('../Libs/synonyms');
+    synonyms.forEach(function(synonym){
+      if(synonym.keyWord == req.params.keyword){
+        res.render('Training/synonymsEdit', { synonym, focus: 2 });
       }
     });
   });    
