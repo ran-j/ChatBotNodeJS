66d1495db587f84c21576ab013ada41a06c1b2dc
Ran-j <ran-junior@hotmail.com>
2019-11-05 22:36:30 -0300

    Update dependence and change seed

--- seed/index.js
+++ seed/index.js
@@ -12,13 +12,9 @@ mongoose
         let intents = require('./intents')
         let synonym = require('./synonyms')
 
-        Promise.all(intents.forEach(async int => {
-            await new intentsModels(int).save().then(() => console.log('saved intent')).catch(console.error)
-        }))
+        Promise.all(intents.forEach(async int => await new intentsModels(int).save().then(() => console.log('saved intent')).catch(console.error)))
 
-        Promise.all(synonym.forEach(async syn => {
-            await new synonymModel(syn).save().then(() => console.log('saved synonym')).catch(console.error)
-        }))
+        Promise.all(synonym.forEach(async syn => await new synonymModel(syn).save().then(() => console.log('saved synonym')).catch(console.error)))
         
         console.log('done')
     }).catch(err => console.log(err)); 
